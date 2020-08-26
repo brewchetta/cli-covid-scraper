f654c1fd33b100b186725d8716b5dc3648f82299
brewchetta <chett.tiller@gmail.com>
2020-08-26 15:35:38 -0400

    Massive styling and allowed arguments to give information for specific dates

--- lib/viewer.rb
+++ lib/viewer.rb
@@ -5,9 +5,19 @@ class Viewer
   end
 
   def run
+    if ARGV.length > 0
+      ARGV.each { |state| display_state(state) }
+    else
+      display_general_info
+    end
+  end
+
+  def display_general_info
+    puts "\n================".colorize(:blue)
     display_country
     display_highest_by
     display_last_checked
+    puts "================\n".colorize(:blue)
   end
 
   def display_highest_by
@@ -23,12 +33,27 @@ class Viewer
   end
 
   def display_country
-    puts "USA => Total positive: #{State.total(:positive).commas} | Total deaths: #{State.total(:deaths).commas} | Percent Positive: #{(State.total(:positive).to_f / State.total(:population) * 100).round(3)}%".colorize :blue
+    puts "USA =>" + " Total positive: #{State.total(:positive).commas} ".colorize(:red) + "|" + " Total deaths: #{State.total(:deaths).commas} ".colorize(:red) + "|" + " Percent Positive: #{(State.total(:positive).to_f / State.total(:population) * 100).round(3)}%".colorize(:red)
     puts "Average => " + "Positives: #{State.avg(:positive).to_i.commas} ".colorize(:red) + "|" + " Deaths: #{State.avg(:deaths).to_i.commas}".colorize(:red)
   end
 
   def display_last_checked
     puts "Last updated #{State.today.min_by(&:last_updated).last_updated.to_s.colorize :red} hours ago"
+  end
+
+  def display_state(state_name)
+    use_commas = Proc.new {|column| column.class == Integer ? column.commas : column }
+    if state = State.find_by(state_initials: state_name.upcase, date_recorded: TODAY)
+      puts "\n================".colorize(:blue)
+      State.column_names.slice(1,100).each do |column|
+        puts "#{column.upcase.sub("_", " ")}: " + "#{use_commas.call(state.send(column))}".colorize(:red)
+      end
+      puts "================\n".colorize(:blue)
+    else
+      puts "\n================".colorize(:blue)
+      puts "Couldn't find " + state_name.colorize(:red)
+      puts "================\n".colorize(:blue)
+    end
   end
 
 end
